8a4df191e3e113cff665868383d43c5dc1e72ad7
cpp_project1 <kdpadaliya216@gmail.com>
2024-06-27 21:02:57 +0530

    Commit

--- lib/view/splashScreen.dart
+++ lib/view/splashScreen.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 import 'package:bhagavad_gita/view/homePage.dart'; // Update with your home page path
@@ -10,49 +11,47 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
-  late Animation<double> _animation;
-  late Animation<Color?> _colorAnimation;
   late Animation<Offset> _textAnimation;
+  late Animation<Color?> _textColorAnimation;
 
-  VideoPlayerController? _videoController;
+  late VideoPlayerController _videoController;
 
   @override
   void initState() {
     super.initState();
 
     _controller = AnimationController(
-      duration: const Duration(seconds: 5),
+      duration: const Duration(milliseconds: 800),
       vsync: this,
     )..forward();
-
-    _animation = CurvedAnimation(
-      parent: _controller,
-      curve: Curves.easeInOut,
-    );
-
-    _colorAnimation = ColorTween(
-      begin: Colors.white,
-      end: Colors.red.shade50,
-    ).animate(_controller);
 
     _textAnimation = Tween<Offset>(
       begin: Offset(0.0, -1.0), // Off-screen to top
       end: Offset.zero, // Final position
     ).animate(CurvedAnimation(
       parent: _controller,
-      curve: Curves.easeInOut,
+      curve: Curves.easeIn,
     ));
 
-    _controller.addStatusListener((status) {
-      if (status == AnimationStatus.completed) {
-        _navigateToHomePage();
-      }
-    });
+    _textColorAnimation = ColorTween(
+      begin: Colors.red,
+      end: Colors.black,
+    ).animate(_controller);
 
-    _videoController = VideoPlayerController.network(
-      'https://cdnl.iconscout.com/lottie/premium/preview-watermark/lord-krishna-5863584-4921684.mp4', // Replace with your actual video URL
+    _videoController = VideoPlayerController.networkUrl(
+      Uri(
+          path:
+              'https://cdnl.iconscout.com/lottie/premium/preview-watermark/lord-krishna-5863584-4921684.mp4'), // Replace with your actual video URL
     )..initialize().then((_) {
-      setState(() {});
+        // Ensure the video starts playing automatically and loops
+        _videoController.play();
+        _videoController.setLooping(true);
+        setState(() {});
+      });
+
+    // Navigate to HomePage after the specified duration
+    Timer(Duration(seconds: 7), () {
+      _navigateToHomePage();
     });
   }
 
@@ -63,79 +62,63 @@ class _SplashScreenState extends State<SplashScreen>
   @override
   void dispose() {
     _controller.dispose();
-    _videoController?.dispose();
+    _videoController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: AnimatedBuilder(
-          animation: _controller,
-          builder: (context, child) {
-            return Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                SlideTransition(
-                  position: _textAnimation,
-                  child: Container(
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomRight,
-                        colors: [
-                          _colorAnimation.value ?? Colors.white,
-                          _colorAnimation.value ?? Colors.red.withOpacity(0.5),
-                        ],
-                      ),
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.3),
-                          blurRadius: 10,
-                          offset: Offset(0, 5),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          // Video Player
+          Padding(
+            padding: const EdgeInsets.only(top: 150),
+            child: Container(
+              height: 420, // Adjust the height as per your requirement
+              child: _videoController.value.isInitialized
+                  ? AspectRatio(
+                      aspectRatio: _videoController.value.aspectRatio,
+                      child: VideoPlayer(_videoController),
+                    )
+                  : Container(), // Placeholder for video player before initialization
+            ),
+          ),
+
+          // Animated Text
+          SlideTransition(
+            position: _textAnimation,
+            child: SafeArea(
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                    text: 'श्रीमद् ',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: _textColorAnimation.value,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: 'भगवद्गीता',
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: _textColorAnimation.value == Colors.red
+                              ? Colors.black
+                              : Colors.red,
                         ),
-                      ],
-                    ),
-                    child: FadeTransition(
-                      opacity: _animation,
-                      child: AspectRatio(
-                        aspectRatio: _videoController!.value.aspectRatio,
-                        child: VideoPlayer(_videoController!),
                       ),
-                    ),
+                    ],
                   ),
                 ),
-                SizedBox(height: 20),
-                SlideTransition(
-                  position: _textAnimation,
-                  child: RichText(
-                    text: TextSpan(
-                      text: 'श्रीमद् ',
-                      style: TextStyle(
-                        fontSize: 30,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      children: [
-                        TextSpan(
-                          text: 'भगवद्गीता',
-                          style: TextStyle(
-                            fontSize: 30,
-                            color: Colors.red,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            );
-          },
-        ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
